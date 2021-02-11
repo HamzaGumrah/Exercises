f747d6c949a18ff2fd252e0d092405626b58822c
Hamza Gumrah <hamzagumrah@localhost.localdomain>
2021-02-11 11:10:16 +0400

    initial

---
+++ Arrays.c
@@ -0,0 +1,19 @@
+##include<stdio.h>
+int A[5];//bss in memory not initialized
+/*
+memory sections
+Heap  pointers mallocs reallocs
+stack static and global variables
+int main(int argc, char const *argv[]) {
+
+  return 0;
+and code section.
+}
+*/
+int main()
+{
+  int B[2]={1,2};//go to code section in memory,2 bytes for each
+  //initialized so in  data segment
+  printf("\nHello World\n\n");
+  return 0;
+}
