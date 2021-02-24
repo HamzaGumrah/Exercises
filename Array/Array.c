5dc9c22f38a58c3c5063b7d45d972c2a28076624
Hamza Gumrah <hamzagumrah@gmail.com>
2021-02-24 23:07:25 +0400

    adding growth rate , _resize and remove functions

--- Array/Array.c
+++ Array/Array.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 #include "Array.h"
 
 void randomizeArray(Array *self, unsigned int seed)
@@ -22,7 +23,7 @@ void randomizeArray(Array *self, unsigned int seed)
             break;
         }
 
-    self->length = self->capacity - 1;
+    self->length = self->capacity ;
 }
 
 /* prints values in an array*/
@@ -72,7 +73,7 @@ void destructArray(Array *self)
 static void _appendToArray(Array *self)
 {
     if (self->length == self->capacity)
-        return;
+        _resize(self);
     // self->candidate.voidType = value;
     switch (self->type)
     {
@@ -95,35 +96,112 @@ static void _appendToArray(Array *self)
     }
     self->length++;
 }
+static void _insertToArray(Array *self, int index){
+    if (self->length == self->capacity)
+        _resize(self);
+    //shift array elements
+    for (int i = self->length; i > index;i--)
+        switch (self->type)
+        {
+        case INT:
+            self->iPtr[i] = self->iPtr[i - 1];
+            break;
+        case FLOAT:
+            self->fPtr[i] = self->fPtr[i-1];
+            break;
+        case CHAR:
+            self->cPtr[i] = self->cPtr[i-1];
+            break;
+        case DOUBLE:
+            self->dPtr[i] = self->dPtr[i-1];
+            break;
+        default:
+            break;
+        }
+    _setArrayIndex(self,index);
+    self->length++;
+}
+void  _setArrayIndex(Array *self,int index){
+    switch (self->type)
+    {
+    case INT:
+        self->iPtr[index] = *(self->candidate.intType);
+        break;
+    case FLOAT:
+        self->fPtr[index] = *(self->candidate.floatType);
+        break;
+    case CHAR:
+        self->cPtr[index] = *(self->candidate.charType);
+        break;
+    case DOUBLE:
+        self->dPtr[index] = *(self->candidate.doubleType);
+        break;
+    default:
+        break;
+    }
+}
 
+void removeFromArray(Array *self, int index){
+    if(index>=self->length || index < 0)
+        return;
+    for (int i = index; i < self->length-1;i++)
+        switch (self->type)
+        {
+        case INT:
+            self->iPtr[i] = self->iPtr[i+1];
+            //*((int *)value);
+            break;
+        case FLOAT:
+            self->fPtr[i] = self->fPtr[i+1];
+            //*((float *)value);
+            break;
+        case CHAR:
+            self->cPtr[i] = self->cPtr[i+1];
+            break;
+        case DOUBLE:
+            self->dPtr[i] = self->dPtr[i+1];
+            break;
+        default:
+            break;
+        }
+    self->length--;
+}
 int main()
 {
-    Array arr = newArray(5, INT);
-    randomizeArray(&arr, time(0));
-    displayArray(&arr);
-    printf("\n");
+    // Array arr = newArray(5, INT);
+    // randomizeArray(&arr, time(0));
+    // displayArray(&arr);
+    // printf("\n");
     Array fArr = newArray(5, FLOAT);
     randomizeArray(&fArr, time(0));
-    displayArray(&fArr);
+    // displayArray(&fArr);
     printf("\n");
-    Array cArr = newArray(5, CHAR);
-    randomizeArray(&cArr, time(0));
-    displayArray(&cArr);
+    // Array cArr = newArray(5, CHAR);
+    // randomizeArray(&cArr, time(0));
+    // displayArray(&cArr);
     
-    destructArray(&arr);
+  /*   destructArray(&arr);
     // destructArray(&fArr);
     destructArray(&cArr);
-    displayArray(&cArr);
+    displayArray(&cArr); */
 
-    Array array = newArray(10, INT);
-    printf("%s\n", DATA_TYPE_STR(array.type));
-    int x = 84;
-    int y = 32;
-    
-    appendToArray(&array, x);
-    appendToArray(&array, y);
-
-    displayArray(&array);
-
+    // Array array = newArray(10, INT);
+    // printf("%s\n", DATA_TYPE_STR(array.type));
+    // int x = 84;
+    // int y = 32;
+    float z = 2.5;
+    // appendToArray(&array, x);
+    // appendToArray(&array, y);
+    // insertToArray(&array, 0, y);
+    // displayArray(&array);
+    appendToArray(&fArr, z);
+ 
+ 
+    displayArray(&fArr);
+    removeFromArray(&fArr, 3);
+    removeFromArray(&fArr, 0);
+    removeFromArray(&fArr, 8);
+    removeFromArray(&fArr, -1);
+    displayArray(&fArr);
     return 0;
 }
--- Array/Array.h
+++ Array/Array.h
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 //#define EMPTY_ARRAY(self) Array self = {.type = UNKOWN,.size = 0, .length = 0}
 #define DATA_TYPE_STR(dataType)                               \
     (INT == dataType ? "int" : (FLOAT == dataType ? "float" : \
     (CHAR == dataType ? "char" : (DOUBLE == dataType ? "double" : "unknown"))))
+
+#define GROWTH_RATE  1.5
 
 typedef enum t_dataType
 {
@@ -12,9 +15,8 @@ typedef enum t_dataType
     CHAR,
     DOUBLE,
     UNKOWN
-}
-DataType;
-//Value converter between data types
+}DataType;
+//Value converter between data typesdsa
 typedef union _candidate{
     void* voidType;
     int* intType;
@@ -35,17 +37,19 @@ typedef struct _Array
         double *dPtr;
     };
     int capacity;
+    int constant;
     int length;
-    float growthRate;
+    double resizeCount;
 } Array;
 /**
  * Constructor for Array
  * */
 // #define newArray(capacity,T)  _newArray(capacity,T);
-static Array newArray(int capacity,DataType type)
+Array newArray(int capacity,DataType type)
 {
     Array newArr;
     newArr.capacity = capacity;
+    newArr.constant = capacity;
     newArr.length = 0;
     newArr.type = type;
     switch (type)
@@ -59,10 +63,34 @@ static Array newArray(int capacity,DataType type)
     case CHAR:
         newArr.cPtr = (char *)malloc(capacity * sizeof(char));
         break;
+    case DOUBLE:
+        newArr.dPtr = (double *)malloc(capacity * sizeof(double));
     default:
         break;
     }
-    return newArr;
+    return newArr; 
+}
+void _resize(Array *self){
+    self->resizeCount++;
+    double multiplier = pow(GROWTH_RATE, self->resizeCount);
+    self->capacity = multiplier*self->constant;
+    switch (self->type)
+    {
+    case INT:
+        self->iPtr = realloc(self->iPtr, self->capacity * sizeof(int));
+        break;
+    case FLOAT:
+        self->fPtr = realloc(self->fPtr, self->capacity * sizeof(float));
+        break;
+    case CHAR:
+        self->cPtr = realloc(self->cPtr,self->capacity * sizeof(char));
+        break;
+    case DOUBLE:
+        self->dPtr = realloc(self->dPtr, self->capacity * sizeof(double));
+        break;
+    default:
+        break;
+    }
 }
 /**
  * generates random values in array
@@ -74,9 +102,16 @@ void displayArray(Array *self);
 
 void destructArray(Array *self);
 
-#define appendToArray(self, value) ({_setCandidate(self,&value); _appendToArray(self); });
+#define appendToArray(self, value) ( {_setCandidate(self,&value); _appendToArray(self); });
 static void _appendToArray(Array *self);
 static void _setCandidate(Array *self, void *value)
 {
     self->candidate.voidType = value;
 }
+
+#define insertToArray(self, index, value) ({_setCandidate(self,&value); _insertToArray(self,index); });
+static void _insertToArray(Array *self, int index);
+void _setArrayIndex(Array *self,int index);
+
+void removeFromArray(Array *self, int index);
+// #define setArrayIndex(index,value)
