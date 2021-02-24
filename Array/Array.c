3e22fcb097258d35dc2b5ef059a1cc569848706a
Hamza Gumrah <hamzagumrah@gmail.com>
2021-02-24 07:02:11 +0400

    working around with unions and macros

--- Array/Array.c
+++ Array/Array.c
@@ -2,31 +2,128 @@
 #include <stdlib.h>
 #include <time.h>
 #include "Array.h"
+
+void randomizeArray(Array *self, unsigned int seed)
+{
+    srand(seed);
+    for (int i = 0; i < self->capacity; i++)
+        switch (self->type)
+        {
+        case INT:
+            self->iPtr[i] = rand();
+            break;
+        case FLOAT:
+            self->fPtr[i] = ((float)rand() / (float)(RAND_MAX)) * 10;
+            break;
+        case CHAR:
+            self->cPtr[i] = 'A' + (random() % 26);
+            break;
+        default:
+            break;
+        }
+
+    self->length = self->capacity - 1;
+}
+
+/* prints values in an array*/
+void displayArray(Array *self)
+{
+    for (int i = 0; i < self->length; i++)
+        switch (self->type)
+        {
+        case INT:
+            printf("[%d] = %d\n", i, self->iPtr[i]);
+            break;
+        case FLOAT:
+            printf("[%d] = %f\n", i, self->fPtr[i]);
+            break;
+        case CHAR:
+            printf("[%d] = %c\n", i, self->cPtr[i]);
+            break;
+        case DOUBLE:
+            printf("[%d] = %f\n", i, self->dPtr[i]);
+            break;
+        default:
+            break;
+        }
+}
+
+void destructArray(Array *self)
+{
+    switch (self->type)
+    {
+    case INT:
+        free(self->iPtr);
+        printf("Deleted INT array\n");
+        break;
+    case FLOAT:
+        free(self->fPtr);
+        printf("Deleted FLOAT array\n");
+        break;
+    case CHAR:
+        free(self->cPtr);
+        printf("Deleted CHAR array\n");
+        break;
+    default:
+        break;
+    }
+    self->length = 0;
+}
+static void _appendToArray(Array *self)
+{
+    if (self->length == self->capacity)
+        return;
+    // self->candidate.voidType = value;
+    switch (self->type)
+    {
+    case INT:
+        self->iPtr[self->length] = *(self->candidate.intType);
+        //*((int *)value);
+        break;
+    case FLOAT:
+        self->fPtr[self->length] = *(self->candidate.floatType);
+        //*((float *)value);
+        break;
+    case CHAR:
+        self->cPtr[self->length] = *(self->candidate.charType);
+        break;
+    case DOUBLE:
+        self->cPtr[self->length] = *(self->candidate.doubleType);
+        break;
+    default:
+        break;
+    }
+    self->length++;
+}
+
 int main()
 {
-    struct Array arr = newArray(5,INT);
+    Array arr = newArray(5, INT);
     randomizeArray(&arr, time(0));
     displayArray(&arr);
     printf("\n");
-    struct Array fArr = newArray(5, FLOAT);
+    Array fArr = newArray(5, FLOAT);
     randomizeArray(&fArr, time(0));
     displayArray(&fArr);
     printf("\n");
-    struct Array cArr = newArray(5, CHAR);
+    Array cArr = newArray(5, CHAR);
     randomizeArray(&cArr, time(0));
     displayArray(&cArr);
     
     destructArray(&arr);
-    destructArray(&fArr);
+    // destructArray(&fArr);
     destructArray(&cArr);
     displayArray(&cArr);
-    EMPTY_ARRAY(array);
-    printf("%s\n", DATA_TYPE_STR(array.type));
-    array = newArray(10, INT);
+
+    Array array = newArray(10, INT);
     printf("%s\n", DATA_TYPE_STR(array.type));
     int x = 84;
+    int y = 32;
+    
+    appendToArray(&array, x);
+    appendToArray(&array, y);
 
-    appendToArray(&array, &x);
     displayArray(&array);
+
     return 0;
 }
--- Array/Array.h
+++ Array/Array.h
@@ -1,48 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define EMPTY_ARRAY(self) Array self = {.type = UNKOWN,.size = 0, .length = 0}
-#define DATA_TYPE_STR(dataType) \
+//#define EMPTY_ARRAY(self) Array self = {.type = UNKOWN,.size = 0, .length = 0}
+#define DATA_TYPE_STR(dataType)                               \
     (INT == dataType ? "int" : (FLOAT == dataType ? "float" : \
-    (CHAR == dataType ? "char" :  "unknown")))
-typedef enum dataType
+    (CHAR == dataType ? "char" : (DOUBLE == dataType ? "double" : "unknown"))))
+
+typedef enum t_dataType
 {
     INT,
     FLOAT,
     CHAR,
+    DOUBLE,
     UNKOWN
-} DataType;
+}
+DataType;
+//Value converter between data types
+typedef union _candidate{
+    void* voidType;
+    int* intType;
+    char *charType;
+    float *floatType;
+    double *doubleType;
+} Candidate;
 
-typedef struct Array
+typedef struct _Array
 {
     DataType type;
+    Candidate candidate;
     union
     {
         int *iPtr;
         float *fPtr;
         char *cPtr;
+        double *dPtr;
     };
-    int size;
+    int capacity;
     int length;
+    float growthRate;
 } Array;
 /**
  * Constructor for Array
  * */
-Array newArray(int size, DataType type)
+// #define newArray(capacity,T)  _newArray(capacity,T);
+static Array newArray(int capacity,DataType type)
 {
     Array newArr;
-    newArr.size = size;
+    newArr.capacity = capacity;
     newArr.length = 0;
     newArr.type = type;
     switch (type)
     {
     case INT:
-        newArr.iPtr = (int *)malloc(size * sizeof(int));
+        newArr.iPtr = (int *)malloc(capacity * sizeof(int));
         break;
     case FLOAT:
-        newArr.fPtr = (float *)malloc(size * sizeof(float));
+        newArr.fPtr = (float *)malloc(capacity * sizeof(float));
         break;
     case CHAR:
-        newArr.cPtr = (char *)malloc(size * sizeof(char));
+        newArr.cPtr = (char *)malloc(capacity * sizeof(char));
         break;
     default:
         break;
@@ -52,86 +67,16 @@ Array newArray(int size, DataType type)
 /**
  * generates random values in array
  * */
-void randomizeArray(Array *self, unsigned int seed)
-{
-    srand(seed);
-    for (int i = 0; i < self->size; i++)
-        switch (self->type)
-        {
-        case INT:
-            self->iPtr[i] = rand();
-            break;
-        case FLOAT:
-            self->fPtr[i] = ((float)rand() / (float)(RAND_MAX)) * 10;
-            break;
-        case CHAR:
-            self->cPtr[i] = 'A' + (random() % 26);
-            break;
-        default:
-            break;
-        }
-
-    self->length = self->size - 1;
-}
+void randomizeArray(Array *self, unsigned int seed);
 
 /* prints values in an array*/
-void displayArray(Array *self)
-{
-    for (int i = 0; i < self->length; i++)
-        switch (self->type)
-        {
-        case INT:
-            printf("[%d] = %d\n", i, self->iPtr[i]);
-            break;
-        case FLOAT:
-            printf("[%d] = %f\n", i, self->fPtr[i]);
-            break;
-        case CHAR:
-            printf("[%d] = %c\n", i, self->cPtr[i]);
-            break;
-        default:
-            break;
-        }
-}
+void displayArray(Array *self);
 
-void destructArray(Array *self)
+void destructArray(Array *self);
+
+#define appendToArray(self, value) ({_setCandidate(self,&value); _appendToArray(self); });
+static void _appendToArray(Array *self);
+static void _setCandidate(Array *self, void *value)
 {
-    switch (self->type)
-    {
-    case INT:
-        free(self->iPtr);
-        printf("Deleted INT array\n");
-        break;
-    case FLOAT:
-        free(self->fPtr);
-        printf("Deleted FLOAT array\n");
-        break;
-    case CHAR:
-        free(self->cPtr);
-        printf("Deleted CHAR array\n");
-        break;
-    default:
-        break;
-    }
-    self->length = 0;
-}
-void appendToArray(Array *self, void* value)
-{
-    if (self->length == self->size)
-        return;
-    switch (self->type)
-    {
-    case INT:
-        self->iPtr[self->length] = *((int *)value);
-        break;
-    case FLOAT:
-        self->fPtr[self->length] = *((float *)value);
-        break;
-    case CHAR:
-        self->cPtr[self->length] = *((char *)value);
-        break;
-    default:
-        break;
-    }
-    self->length++;
+    self->candidate.voidType = value;
 }
